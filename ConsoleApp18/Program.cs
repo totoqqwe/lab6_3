53d62ff6cd580060531255e85a1871fb338299e0
totoqqwe <anton.stepanchenko14@gmail.com>
2023-11-23 20:36:47 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quaternion q1 = new Quaternion(1, 2, 3, 4);
+            Quaternion q2 = new Quaternion(5, 6, 7, 8);
+            Quaternion sum = q1 + q2;
+
+            Console.WriteLine($"Sum: {sum.W}, {sum.X}, {sum.Y}, {sum.Z}");
+
+            Quaternion difference = q1 - q2;
+            Console.WriteLine($"Difference: {difference.W}, {difference.X}, {difference.Y}, {difference.Z}");
+
+            Quaternion product = q1 * q2;
+            Console.WriteLine($"Product: {product.W}, {product.X}, {product.Y}, {product.Z}");
+
+            double norm = q1.Norm();
+            Console.WriteLine($"Norm of q1: {norm}");
+
+            Quaternion conjugate = q1.Conjugate();
+            Console.WriteLine($"Conjugate of q1: {conjugate.W}, {conjugate.X}, {conjugate.Y}, {conjugate.Z}");
+
+            Quaternion inverse = q1.Inverse();
+            Console.WriteLine($"Inverse of q1: {inverse.W}, {inverse.X}, {inverse.Y}, {inverse.Z}");
+
+            Console.WriteLine($"Are q1 and q2 equal? {q1 == q2}");
+            Console.WriteLine($"Are q1 and q2 not equal? {q1 != q2}");
+
+            double[,] rotationMatrix = q1.ToRotationMatrix();
+            Console.WriteLine("Rotation Matrix:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{rotationMatrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ConsoleApp18/Quaternion.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Quaternion
+    {
+        public double W { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Quaternion(double w, double x, double y, double z)
+        {
+            W = w;
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public static Quaternion operator +(Quaternion q1, Quaternion q2)
+        {
+            return new Quaternion(q1.W + q2.W, q1.X + q2.X, q1.Y + q2.Y, q1.Z + q2.Z);
+        }
+
+        public static Quaternion operator -(Quaternion q1, Quaternion q2)
+        {
+            return new Quaternion(q1.W - q2.W, q1.X - q2.X, q1.Y - q2.Y, q1.Z - q2.Z);
+        }
+
+        public static Quaternion operator *(Quaternion q1, Quaternion q2)
+        {
+            double w = q1.W * q2.W - q1.X * q2.X - q1.Y * q2.Y - q1.Z * q2.Z;
+            double x = q1.W * q2.X + q1.X * q2.W + q1.Y * q2.Z - q1.Z * q2.Y;
+            double y = q1.W * q2.Y - q1.X * q2.Z + q1.Y * q2.W + q1.Z * q2.X;
+            double z = q1.W * q2.Z + q1.X * q2.Y - q1.Y * q2.X + q1.Z * q2.W;
+
+            return new Quaternion(w, x, y, z);
+        }
+
+        public double Norm()
+        {
+            return Math.Sqrt(W * W + X * X + Y * Y + Z * Z);
+        }
+
+        public Quaternion Conjugate()
+        {
+            return new Quaternion(W, -X, -Y, -Z);
+        }
+
+        public Quaternion Inverse()
+        {
+            double normSquared = W * W + X * X + Y * Y + Z * Z;
+            if (normSquared == 0)
+                throw new InvalidOperationException("Cannot invert a quaternion with zero norm.");
+
+            double reciprocalNormSquared = 1 / normSquared;
+            return new Quaternion(W * reciprocalNormSquared, -X * reciprocalNormSquared, -Y * reciprocalNormSquared, -Z * reciprocalNormSquared);
+        }
+
+        public static bool operator ==(Quaternion q1, Quaternion q2)
+        {
+            return q1.W == q2.W && q1.X == q2.X && q1.Y == q2.Y && q1.Z == q2.Z;
+        }
+
+        public static bool operator !=(Quaternion q1, Quaternion q2)
+        {
+            return !(q1 == q2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Quaternion other)
+                return this == other;
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return W.GetHashCode() ^ X.GetHashCode() ^ Y.GetHashCode() ^ Z.GetHashCode();
+        }
+
+        public double[,] ToRotationMatrix()
+        {
+            double[,] matrix = new double[3, 3];
+
+            double xx = X * X;
+            double xy = X * Y;
+            double xz = X * Z;
+            double xw = X * W;
+
+            double yy = Y * Y;
+            double yz = Y * Z;
+            double yw = Y * W;
+
+            double zz = Z * Z;
+            double zw = Z * W;
+
+            matrix[0, 0] = 1 - 2 * (yy + zz);
+            matrix[0, 1] = 2 * (xy - zw);
+            matrix[0, 2] = 2 * (xz + yw);
+
+            matrix[1, 0] = 2 * (xy + zw);
+            matrix[1, 1] = 1 - 2 * (xx + zz);
+            matrix[1, 2] = 2 * (yz - xw);
+
+            matrix[2, 0] = 2 * (xz - yw);
+            matrix[2, 1] = 2 * (yz + xw);
+            matrix[2, 2] = 1 - 2 * (xx + yy);
+
+            return matrix;
+        }
+    }
+}
